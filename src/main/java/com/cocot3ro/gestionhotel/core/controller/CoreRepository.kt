2f4aec900d0fb2134dc260d183003fb39526b7e3
cocot3ro <pablooterocastro15@gmail.com>
2024-07-26 01:55:40 +0200

    Ping endpoint para probar conexion

--- src/main/java/com/cocot3ro/gestionhotel/core/controller/CoreRepository.kt
+++ src/main/java/com/cocot3ro/gestionhotel/core/controller/CoreRepository.kt
@@ -1,4 +1,15 @@
 package com.cocot3ro.gestionhotel.core.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api")
 class CoreRepository {
+
+    @GetMapping("/ping")
+    fun ping(): String {
+        return "pong"
+    }
 }
