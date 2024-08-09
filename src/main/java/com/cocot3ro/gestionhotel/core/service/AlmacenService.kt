60dff761b83178125b2f6b2da981793263e8d24d
cocot3ro <pablooterocastro15@gmail.com>
2024-08-09 19:28:05 +0200

    Se cambia API REST por RSocket

--- src/main/java/com/cocot3ro/gestionhotel/core/service/AlmacenService.kt
+++ src/main/java/com/cocot3ro/gestionhotel/core/service/AlmacenService.kt
@@ -1,35 +1,26 @@
-package com.cocot3ro.gestionhotel.core.controller
+package com.cocot3ro.gestionhotel.core.service
 
 import com.cocot3ro.gestionhotel.core.entities.AlmacenEntity
 import com.cocot3ro.gestionhotel.core.persistence.AlmacenJPARepository
-import org.springframework.http.MediaType
-import org.springframework.web.bind.annotation.*
+import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import java.time.Duration
 
-@RestController
-@RequestMapping("/api/almacen")
-class AlmacenRestRepository(private val almacenJPARepository: AlmacenJPARepository) {
+@Service
+class AlmacenService(private val almacenJPARepository: AlmacenJPARepository) {
 
-    @GetMapping(
-        value = ["/all"],
-        produces = [MediaType.TEXT_EVENT_STREAM_VALUE]
-    )
     fun all(): Flux<List<AlmacenEntity>> {
         return Flux.interval(Duration.ofSeconds(1)).map {
             almacenJPARepository.findAll().toList()
         }
     }
 
-    @PostMapping("/save")
-    fun saveAlmacenEntity(@RequestBody almacenEntity: AlmacenEntity): AlmacenEntity {
-        println(almacenEntity)
+    fun save(almacenEntity: AlmacenEntity): AlmacenEntity {
         return almacenJPARepository.save(almacenEntity)
     }
 
-    @PutMapping("/update/{id}")
-    fun updateAlmacenEntity(@RequestBody newAlmacenEntity: AlmacenEntity, @PathVariable id: Long): AlmacenEntity {
-        return almacenJPARepository.findById(id)
+    fun update(newAlmacenEntity: AlmacenEntity): AlmacenEntity {
+        return almacenJPARepository.findById(newAlmacenEntity.id)
             .map { almacenEntity: AlmacenEntity ->
                 almacenEntity.nombre = newAlmacenEntity.nombre
                 almacenEntity.cantidad = newAlmacenEntity.cantidad
@@ -38,13 +29,12 @@ class AlmacenRestRepository(private val almacenJPARepository: AlmacenJPAReposito
                 return@map almacenJPARepository.save(almacenEntity)
             }
             .orElseGet {
-                newAlmacenEntity.id = id
+                newAlmacenEntity.id = 0L
                 return@orElseGet almacenJPARepository.save(newAlmacenEntity)
             }
     }
 
-    @DeleteMapping("/delete/{id}")
-    fun deleteAlmacenEntity(@PathVariable id: Long) {
+    fun delete(id: Long) {
         almacenJPARepository.deleteById(id)
     }
 }
