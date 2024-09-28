56e830b9ed1531b67b793379538e9d7bd052e7ed
cocot3ro <pablooterocastro15@gmail.com>
2024-09-28 15:31:35 +0200

    v1.2.0 -> Missing annotations on rest controllers

--- GestionHotel-Core/src/main/java/com/cocot3ro/gestionhotel/core/controller/rest/CoreRestController.kt
+++ GestionHotel-Core/src/main/java/com/cocot3ro/gestionhotel/core/controller/rest/CoreRestController.kt
@@ -2,11 +2,14 @@ package com.cocot3ro.gestionhotel.core.controller.rest
 
 import com.cocot3ro.gestionhotel.core.controller.ICoreController
 import com.cocot3ro.gestionhotel.core.service.CoreService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController
 class CoreRestController(private val coreService: CoreService) : ICoreController {
+
+    @GetMapping("/status")
     override fun status(): Mono<String> {
         return coreService.status()
     }
