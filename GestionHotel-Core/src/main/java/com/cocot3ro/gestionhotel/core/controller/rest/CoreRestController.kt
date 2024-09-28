d6b7dca651f7324c94e3f377f636d31a0883c53f
cocot3ro <pablooterocastro15@gmail.com>
2024-09-28 15:45:20 +0200

    missing annotation on rest core controller

--- GestionHotel-Core/src/main/java/com/cocot3ro/gestionhotel/core/controller/rest/CoreRestController.kt
+++ GestionHotel-Core/src/main/java/com/cocot3ro/gestionhotel/core/controller/rest/CoreRestController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
-@RestController
+@RestController("/api/core")
 class CoreRestController(private val coreService: CoreService) : ICoreController {
 
     @GetMapping("/status")
